fdadfe29242693c313505639d4fb96f8f31df7eb
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-27 11:44:38 +0530

    FEATURE: create router in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,47 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+
+import Inbox from "@/pages/Inbox";
+import Today from "@/pages/Today";
+import Upcoming from "@/pages/Upcoming";
+
+import { Sidebar } from "@/components/Sidebar";
+
+const Layout = () => {
+  return (
+    <div className="flex">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Inbox />,
+      },
+      {
+        path: "/today",
+        element: <Today />,
+      },
+      {
+        path: "/upcoming",
+        element: <Upcoming />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <div className="text-red-500">Todo</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
