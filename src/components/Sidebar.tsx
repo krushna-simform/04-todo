a53b3b82b4a560a4c0edebdb747c16356650e96c
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-27 11:44:00 +0530

    FEATURE: create Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,64 @@
+import { cn } from "clsx-for-tailwind";
+import { Link, useLocation } from "react-router";
+import type { SidebarLinks } from "@/types/type";
+
+import addTaskIcon from "/icons/add-task-1.svg";
+import inboxIcon from "/icons/inbox.svg";
+import todayIcon from "/icons/today.svg";
+import upcomingIcon from "/icons/upcoming.svg";
+
+const sidebarLinks: Array<SidebarLinks> = [
+  {
+    id: crypto.randomUUID(),
+    label: "Inbox",
+    to: "/",
+    src: inboxIcon,
+  },
+  {
+    id: crypto.randomUUID(),
+    label: "Today",
+    to: "/today",
+    src: todayIcon,
+  },
+  {
+    id: crypto.randomUUID(),
+    label: "Upcoming",
+    to: "/upcoming",
+    src: upcomingIcon,
+  },
+];
+
+export const Sidebar = () => {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  return (
+    <div className="h-[100vh] min-w-[300px] pl-6 py-4 bg-sidebarBackground">
+      <p className="flex items-center gap-2 py-2 px-3 rounded-l-lg cursor-pointer hover:bg-secondaryColor transition-colors ease-in-out">
+        <img src={addTaskIcon} alt="Add task" className="h-7" />
+        <span className="text-primaryColor text-lg font-semibold">
+          Add task
+        </span>
+      </p>
+      <nav className="mt-3 flex flex-col gap-2">
+        <ul className="space-y-2">
+          {sidebarLinks.map((item) => (
+            <li key={item.id}>
+              <Link
+                to={item.to}
+                className={cn(
+                  "flex gap-2 px-3 py-2 cursor-pointer rounded-l-lg hover:bg-secondaryColor hover:text-primaryColor",
+                  currentPath === `${item.to}` &&
+                    "bg-secondaryColor text-primaryColor"
+                )}
+              >
+                <img src={item.src} alt="Inbox" className="h-7" />
+                <p className="text-lg">{item.label}</p>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+};
