adcd746cb915e7264541415f06b682da62accef7
krushna rathod <krushna.rathod@simformsolutions.com>
2025-05-30 12:37:46 +0530

    FEATURE: add AddTodo and ImgShowcase component in Inbox, Today and Upcoming page

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -1,7 +1,20 @@
+import { AddTodo } from "@/components/AddTodo";
+import { ImgShowcase } from "@/components/ui/ImgShowcase";
+
 const Today = () => {
   return (
-    <div>
-      <h1>Today</h1>
+    <div className="w-full">
+      <div className="w-[50%] mx-auto pt-18">
+        <p className="text-2xl font-bold" role="heading">
+          Today
+        </p>
+
+        <div className="mt-6">
+          <AddTodo />
+        </div>
+
+        <ImgShowcase />
+      </div>
     </div>
   );
 };
