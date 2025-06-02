5a14217464e6af979ba777b9f355506990d33625
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-02 12:50:23 +0530

    FEATURE: create todoSlice

---
+++ src/redux/todoSlice.ts
@@ -0,0 +1,47 @@
+import { createSlice, type PayloadAction } from "@reduxjs/toolkit";
+import type { Todo } from "@/types/type";
+
+interface TodoState {
+  todos: Array<Todo>;
+}
+
+const savedTodos = JSON.parse(localStorage.getItem("todos") || "[]");
+
+const initialState: TodoState = {
+  todos: Array.isArray(savedTodos) ? savedTodos : [],
+};
+
+const saveToLocalStorage = (todos: Array<Todo>) => {
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<Todo>) => {
+      state.todos.push(action.payload);
+      saveToLocalStorage(state.todos);
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+      saveToLocalStorage(state.todos);
+    },
+    toggleComplete: (state, action: PayloadAction<string>) => {
+      const todo = state.todos.find((t) => t.id === action.payload);
+      if (todo) todo.completed = !todo.completed;
+      saveToLocalStorage(state.todos);
+    },
+    editTodo: (state, action: PayloadAction<Todo>) => {
+      const index = state.todos.findIndex((t) => t.id === action.payload.id);
+      if (index !== -1) {
+        state.todos[index] = action.payload;
+      }
+      saveToLocalStorage(state.todos);
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleComplete, editTodo } =
+  todoSlice.actions;
+export default todoSlice.reducer;
