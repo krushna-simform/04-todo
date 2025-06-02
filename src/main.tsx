580785ae276aefdb98951b7590aeb52da3bfa7e7
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-02 12:51:34 +0530

    FEATURE: create sotre for totoSlice and add wrap app in store

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
 import { TodoProvider } from "@/context/TodoContext";
+
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <TodoProvider>
-      <App />
-    </TodoProvider>
+    <Provider store={store}>
+      <TodoProvider>
+        <App />
+      </TodoProvider>
+    </Provider>
   </StrictMode>
 );
