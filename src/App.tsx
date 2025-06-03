b5b6a3f2081e7ed8216e895809cadd97fe4689eb
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-03 16:21:54 +0530

    FEATURE: provide fix height to main content

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const Layout = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <main className="flex-1 h-[100vh] overflow-y-auto">
+      <main className="flex-1 h-[100vh]">
         <Outlet />
       </main>
     </div>
