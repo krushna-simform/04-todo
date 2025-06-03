daf8eced84baa0a515424627bf791972cba9dcd5
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-03 16:23:13 +0530

    FEATURE: update Inbox page, and make todo list section scrollable

--- src/pages/Inbox.tsx
+++ src/pages/Inbox.tsx
@@ -8,17 +8,22 @@ import { Todos } from "@/components/Todos";
 const Inbox = () => {
   const todos = useSelector((state: RootState) => state.todo.todos);
   return (
-    <div className="w-full">
-      <div className="w-[90%] md:w-[50%] mx-auto pt-18">
-        <p className="text-2xl font-bold" role="heading">
-          Inbox
-        </p>
+    <div className="w-full h-full">
+      <div className="w-[90%] md:w-[50%] mx-auto pt-18 flex flex-col h-full">
+        <div>
+          <p className="text-2xl font-bold" role="heading">
+            Inbox
+          </p>
 
-        <div className="mt-6">
-          <AddTodo />
+          <div className="mt-6">
+            <AddTodo />
+          </div>
         </div>
 
-        <div className="mt-4">
+        <div
+          className="mt-4 h-full flex-1 overflow-y-auto"
+          style={{ scrollbarWidth: "none" }}
+        >
           {todos.map((todos) => (
             <Todos key={todos.id} todo={todos} />
           ))}
