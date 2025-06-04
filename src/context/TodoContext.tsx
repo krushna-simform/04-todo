104f1935a217b390884bec19e81b9abb7717b76d
krushna rathod <krushna.rathod@simformsolutions.com>
2025-06-04 13:08:32 +0530

    FEATURE: update todocontext for detials-todo id

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -3,8 +3,10 @@ import { createContext, useState, type ReactNode } from "react";
 interface TodoContextType {
   isAddTodoOpen: boolean;
   editingTodoId: string | null;
+  detailTodoId: string | null;
   handleAddTodoClick: () => void;
   setEditingTodoId: (id: string | null) => void;
+  setDetailTodoId: (id: string | null) => void;
 }
 
 interface TodoContextProp {
@@ -16,6 +18,7 @@ const TodoContext = createContext<TodoContextType | undefined>(undefined);
 const TodoProvider = ({ children }: TodoContextProp) => {
   const [isAddTodoOpen, setIsTodoOpen] = useState(false);
   const [editingTodoId, setEditingTodoId] = useState<string | null>(null);
+  const [detailTodoId, setDetailTodoId] = useState<string | null>(null);
 
   const handleAddTodoClick = () => setIsTodoOpen((prev) => !prev);
 
@@ -24,8 +27,10 @@ const TodoProvider = ({ children }: TodoContextProp) => {
       value={{
         isAddTodoOpen,
         editingTodoId,
+        detailTodoId,
         handleAddTodoClick,
         setEditingTodoId,
+        setDetailTodoId,
       }}
     >
       {children}
